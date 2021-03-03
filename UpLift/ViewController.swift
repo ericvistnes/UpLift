4ed15de1779cf7c842cbcf62d8b99a7f5ba1e9aa
Eric Vistnes <evistnes23@gmail.com>
2021-03-03 09:01:03 -0600

    Initial Commit

---
+++ UpLift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UpLift
+//
+//  Created by Eric Vistnes on 3/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
